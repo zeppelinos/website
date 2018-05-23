da08f9a8cdf3b213780c560b48521a1e138f8f64
Delfina <delfinapolito@gmail.com>
2018-05-23 20:07:52 -0300

    animation

--- js/home.js
+++ js/home.js
@@ -5,4 +5,38 @@ $( document ).ready(function(){
 	});
   
   $('.modal').modal();
+
+
+  	var images = [
+			"./img/animation/imagen1_1.svg",
+			"./img/animation/imagen1_2.svg",
+			"./img/animation/imagen1_3.svg",
+			"./img/animation/imagen1_4.svg",
+			"./img/animation/imagen2.svg",
+		];
+	// TweenMax can tween any property of any object. We use this object to cycle through the array
+	var obj = {curImg: 0};
+
+	// create tween
+	var tween = TweenMax.to(obj, 0.5,
+		{
+			curImg: images.length - 1,	// animate propery curImg to number of images
+			roundProps: "curImg",				// only integers so it can be used as an array index
+			repeat: 0,									// repeat 1 time
+			ease: Linear.easeNone,			// show every image the same ammount of time
+			onUpdate: function () {
+			  $("#image1").attr("src", images[obj.curImg]); // set the image source
+			}
+		}
+	);
+
+  var controller = new ScrollMagic.Controller();
+  var scene = new ScrollMagic.Scene({
+  	triggerElement: "#trigger", duration: 400})
+			.setTween(tween)
+			.addTo(controller);
+
+
 })
+
+
