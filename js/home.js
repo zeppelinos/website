416fdf538a870076deda47b17e18bcc3a6ea2f05
Delfina <delfinapolito@gmail.com>
2018-05-24 16:54:50 -0300

    general fixes

--- js/home.js
+++ js/home.js
@@ -6,34 +6,6 @@ $( document ).ready(function(){
   
   $('.modal').modal();
 
-
-	var images = [
-		"./img/animation/imagen1_1.svg",
-		"./img/animation/imagen1_2.svg",
-		"./img/animation/imagen1_3.svg",
-		"./img/animation/imagen1_4.svg",
-		"./img/animation/imagen2.svg",
-	];
-	var obj = {curImg: 0};
-	var tween = TweenMax.to(obj, 0.5,
-		{
-			curImg: images.length - 1,	// animate propery curImg to number of images
-			roundProps: "curImg",				// only integers so it can be used as an array index
-			repeat: 0,									// repeat 1 time
-			ease: Linear.easeNone,			// show every image the same ammount of time
-			onUpdate: function () {
-			  $("#image1").attr("src", images[obj.curImg]); // set the image source
-			}
-		}
-	);
-
-  var controller = new ScrollMagic.Controller();
-  var scene = new ScrollMagic.Scene({
-  	triggerElement: "#trigger", duration: 200})
-			.setTween(tween)
-			.addTo(controller);
-
-
 })
 
 
