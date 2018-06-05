0660f6bd5d81adaccf45d3ad1bb605e428ef297b
Delfina <delfinapolito@gmail.com>
2018-06-05 16:28:57 -0300

    fix on hero animation

--- js/home.js
+++ js/home.js
@@ -5,6 +5,28 @@ $( document ).ready(function(){
 	});
   
   $('.modal').modal();
+  $('.hero-img').removeClass('hidden');
+  
+  $(window).scroll(function() {
+	  var features = $('.features').offset().top;
+	  var projects = $('.projects-title').offset().top;
+
+	  if ($(window).scrollTop() >= (features - 400)){
+	    $('.feature1').addClass('animated fadeInDown');
+	    $('.feature1').removeClass('hidden');
+	    setTimeout(function () {
+	      $('.feature2').addClass('animated fadeInDown');
+	      $('.feature2').removeClass('hidden');
+	    }, 500);
+	    setTimeout(function () {
+        $('.feature3').addClass('animated fadeInDown');
+        $('.feature3').removeClass('hidden');
+      }, 1000);
+	  }
+	  if ($(window).scrollTop() >= (projects - 400)){
+	    $('.project-img').removeClass('hidden');
+	  }
+	});
 
 })
 
